ffc0dae4dd5f474b8aa0955ff93d2ecb989ba3d8
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-08-30 00:49:22 -0500

    Set up documents and added basic methods for calculator

---
+++ calc_script.js
@@ -0,0 +1,26 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    if (operator.trim() === '+')
+        return add(num1, num2);
+    else if (operator.trim === '-')
+        return subtract(num1, num2);
+    else if (operator.trim === '*')
+        return multiply(num1, num2);
+    else if (operator.trim === '/')
+        return divide(num1, num2);
+}
