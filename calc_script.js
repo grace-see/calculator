6a270495bfbbc60bb1a8a225c29ad7bd77eab36d
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-09-11 00:55:54 -0500

    Calculator is able to calculate expressions

--- calc_script.js
+++ calc_script.js
@@ -1,4 +1,4 @@
-function add (num1, num2) {
+function add(num1, num2) {
     return num1 + num2;
 }
 
@@ -12,13 +12,16 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     if (num2 === 0)
-        return "CANNOT DIVIDE BY ZERO";
+        return "CANNOT DIVIDE BY ZERO >:(";
     return num1 / num2;
 }
 
 function operate(expression) {
     //parse the expression to get each element
-    let operator, num1, num2;
+    let operatorIndex = expression.search(/\+|\-|\*|\//);
+    let operator = expression[operatorIndex];
+    let num1 = Number(expression.substring(0, operatorIndex));
+    let num2 = Number(expression.substring(operatorIndex+1, expression.length));
     if (operator.trim() === '+')
         return add(num1, num2);
     else if (operator.trim() === '-')
@@ -29,86 +32,83 @@ function operate(expression) {
         return divide(num1, num2);
 }
 
-function setUpButtons() {
-    //get the buttons & add event listeners
-    container.querySelector("#one").addEventListener("click", () => {
-        display.value = display.value + "1";
-    });
-
-    container.querySelector("#two").addEventListener("click", () => {
-        display.value = display.value + "2";
-    });
-
-    container.querySelector("#three").addEventListener("click", () => {
-        display.value = display.value + "3";
-    });
-
-    container.querySelector("#four").addEventListener("click", () => {
-        display.value = display.value + "4";
-    });
-
-    container.querySelector("#five").addEventListener("click", () => {
-        display.value = display.value + "5";
-    });
-
-    container.querySelector("#six").addEventListener("click", () => {
-        display.value = display.value + "6";
-    });
-
-    container.querySelector("#seven").addEventListener("click", () => {
-        display.value = display.value + "7";
-    });
-
-    container.querySelector("#eight").addEventListener("click", () => {
-        display.value = display.value + "8";
-    });
-
-    container.querySelector("#nine").addEventListener("click", () => {
-        display.value = display.value + "9";
-    });
-
-    container.querySelector("#zero").addEventListener("click", () => {
-        display.value = display.value + "0";
-    });
-
-    container.querySelector("#clear").addEventListener("click", () => {
-        display.value = "";
-    });
-
-    container.querySelector("#add").addEventListener("click", () => {
-        if (display.value.search(/\+|\-|\*|\//) !== -1)
-            display.value = operate(display.value) + "+";
-        else
-            display.value = display.value + "+";
-    });
-
-    container.querySelector("#subtract").addEventListener("click", () => {
-        if (display.value.search(/\+|\-|\*|\//) !== -1)
-            display.value = operate(display.value) + "-";
-        display.value = display.value + "-";
-    });
-
-    container.querySelector("#multiply").addEventListener("click", () => {
-        if (display.value.search(/\+|\-|\*|\//) !== -1)
-            display.value = operate(display.value) + "*";
-        display.value = display.value + "*";
-    });
-
-    container.querySelector("#divide").addEventListener("click", () => {
-        if (display.value.search(/\+|\-|\*|\//) !== -1)
-            display.value = operate(display.value) + "/";
-        display.value = display.value + "/";
-    });
-
-    container.querySelector("#equal").addEventListener("click", () => {
-        display.value = operate(display.value);
-    });
-}
-
-let num1, num2, operator;
-
 const container = document.querySelector(".container");
 
 const display = container.querySelector("#text-field");
 
-setUpButtons();
+//get the buttons & add event listeners
+container.querySelector("#one").addEventListener("click", () => {
+    display.value = display.value + "1";
+});
+
+container.querySelector("#two").addEventListener("click", () => {
+    display.value = display.value + "2";
+});
+
+container.querySelector("#three").addEventListener("click", () => {
+    display.value = display.value + "3";
+});
+
+container.querySelector("#four").addEventListener("click", () => {
+    display.value = display.value + "4";
+});
+
+container.querySelector("#five").addEventListener("click", () => {
+    display.value = display.value + "5";
+});
+
+container.querySelector("#six").addEventListener("click", () => {
+    display.value = display.value + "6";
+});
+
+container.querySelector("#seven").addEventListener("click", () => {
+    display.value = display.value + "7";
+});
+
+container.querySelector("#eight").addEventListener("click", () => {
+    display.value = display.value + "8";
+});
+
+container.querySelector("#nine").addEventListener("click", () => {
+    display.value = display.value + "9";
+});
+
+container.querySelector("#zero").addEventListener("click", () => {
+    display.value = display.value + "0";
+});
+
+container.querySelector("#clear").addEventListener("click", () => {
+    display.value = "";
+});
+
+container.querySelector("#add").addEventListener("click", () => {
+    if (display.value.search(/\+|\-|\*|\//) !== -1)
+         display.value = operate(display.value) + "+";
+    else
+        display.value = display.value + "+";
+});
+
+container.querySelector("#subtract").addEventListener("click", () => {
+    if (display.value.search(/\+|\-|\*|\//) !== -1)
+        display.value = operate(display.value) + "-";
+    else
+        display.value = display.value + "-";
+});
+
+container.querySelector("#multiply").addEventListener("click", () => {
+    if (display.value.search(/\+|\-|\*|\//) !== -1)
+        display.value = operate(display.value) + "*";
+    else
+        display.value = display.value + "*";
+});
+
+container.querySelector("#divide").addEventListener("click", () => {
+    if (display.value.search(/\+|\-|\*|\//) !== -1)
+        display.value = operate(display.value) + "/";
+    else
+        display.value = display.value + "/";
+});
+
+container.querySelector("#equal").addEventListener("click", () => {
+    display.value = operate(display.value);
+});
